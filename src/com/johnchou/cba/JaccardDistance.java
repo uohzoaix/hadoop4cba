35ba774bc4235a48f5bbc5240aa46ac1d70d60c6
johnchou <johnchou66@gmail.com>
2014-03-04 15:01:42 +0800

    使用hadoop进行用户行为分析

--- src/com/johnchou/cba/JaccardDistance.java
+++ src/com/johnchou/cba/JaccardDistance.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
+ 
 public class JaccardDistance {
 	public static Map<String, Double> weightMap = new HashMap<String, Double>();
 
